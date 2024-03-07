f4a674650dd932cd88ed7b73f9915575988e88bd
BlindCoco21 <alfiahts00@gmail.com>
2024-03-07 08:29:29 +0700

    new folder

---
+++ BE_RPL/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+
+const express = require('express')
+const app = express()
+app.use(cors())
+app.use(express.json())
+const port = 5000
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+app.get('/ProjekRPL', (req, res) => {
+  console.log({urlParam :req.query})
+  res.send('GACOR KANG BINGUNG GWAH')
+})
+
+app.post('/login', (req, res) =>{
+  console.log({requestFromOutside : req.body})
+  res.send('Login berhasil cuy')
+})
+
+app.put('/username', (req,res) =>{
+  console.log({updateData : req.body})
+  res.send('update berhasil')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
