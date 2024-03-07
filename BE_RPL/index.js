a62fed91861dd2b69e786ff5563c923e7f3675a8
BlindCoco21 <alfiahts00@gmail.com>
2024-03-07 11:11:06 +0700

    commit 4

--- BE_RPL/index.js
+++ BE_RPL/index.js
@@ -1,32 +1,29 @@
 import express from "express";
 import cors from "cors";
-import UserRoute from "./routes/UserRoute.js"
+import UserRoute from "./routes/UserRoute.js";
 
-const express = require('express')
-const app = express()
-app.use(cors())
-app.use(express.json())
-const port = 5000
+const app = express();
+const port = 5000;
 
-const bodyParser = require('body-parser')
-
-app.use(bodyParser.json())
+app.use(cors());
+app.use(express.json());
+app.use(UserRoute);
 
 app.get('/ProjekRPL', (req, res) => {
-  console.log({urlParam :req.query})
-  res.send('GACOR KANG BINGUNG GWAH')
-})
+  console.log({urlParam : req.query});
+  res.send('GACOR KANG BINGUNG GWAH');
+});
 
 app.post('/login', (req, res) =>{
-  console.log({requestFromOutside : req.body})
-  res.send('Login berhasil cuy')
-})
+  console.log({requestFromOutside : req.body});
+  res.send('Login berhasil cuy');
+});
 
-app.put('/username', (req,res) =>{
-  console.log({updateData : req.body})
-  res.send('update berhasil')
-})
+app.put('/username', (req, res) =>{
+  console.log({updateData : req.body});
+  res.send('update berhasil');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
