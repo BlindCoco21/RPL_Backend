6eb04af812c26e6c4a9d03d677edbc1bcd282fbe
Lutfizp <Lutfizp@gmail.com>
2024-03-24 12:50:40 +0700

    Fecthing update (kurang paham bener apa nggak,coba cek di postman)

--- BE_RPL/config/Database.js
+++ BE_RPL/config/Database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("Serambi_Buku", "root", "", {
+const db = new Sequelize("Serambi_Buku", "postgres", "@mentoz05", {
   host: "localhost",
-  dialect: "mysql",
+  dialect: "postgres",
 });
 
 export default db;
